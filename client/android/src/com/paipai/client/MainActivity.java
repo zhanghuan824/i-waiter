edbed076e91391fdb3409373b0ab11eb12786a19
Huan Zhang <zh_bjut@163.com>
2014-01-12 17:43:01 +0800

    add zxing library and its sample

--- client/android/src/com/paipai/client/MainActivity.java
+++ client/android/src/com/paipai/client/MainActivity.java
@@ -2,7 +2,6 @@ package com.paipai.client;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
